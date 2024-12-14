7b211b687cb6902073f0029bb2f5992586545111
Abdur Rahman <60320088+Abdurahmanm2020@users.noreply.github.com>
2024-12-14 18:39:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function revealButton() {
 }
 
 reveal.onclick = () => revealButton();
+
+// Changing text
+
+var skills = ['UI/UX', 'Graphics', 'Apps', 'Logo', 'Product', 'Webflow'];
+            
+textSequence(0);
+function textSequence(i) {
+
+    if (skills.length > i) {
+        setTimeout(function() {
+            document.getElementById("swap-text").innerHTML = skills[i];
+            textSequence(++i);
+        }, 1000); 
+
+    } else if (skills.length == i) { 
+        textSequence(0);
+    }
+
+}
